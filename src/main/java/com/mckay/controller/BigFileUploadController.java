3fe219ce71b8349b86823757812611817c8f2a11
mckayzhou <buctzhoulinbo@163.com>
2017-01-15 18:53:10 +0800

    大文件上传控制器

---
+++ src/main/java/com/mckay/controller/BigFileUploadController.java
@@ -0,0 +1,22 @@
+/**   
+ * Copyright © 2017 本代码版权归周林波所有，严禁未经许可使用。
+ * 
+ * @Title: BigFileUploadController.java 
+ * @Prject: webplatform
+ * @Package: com.mckay.controller 
+ * @Description:
+ * @author:
+ * @date: 2017年1月13日 下午9:35:03 
+ * @version: V1.0   
+ */
+package com.mckay.controller;
+
+/** 
+ * @ClassName: BigFileUploadController 
+ * @Description: TODO
+ * @author: 周林波
+ * @date: 2017年1月13日 下午9:35:03  
+ */
+public class BigFileUploadController {
+	
+}
