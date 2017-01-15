e73b88ac8bb247f778ba3928f1701b2345ae2e19
mckayzhou <buctzhoulinbo@163.com>
2017-01-15 18:57:09 +0800

    大文件处理服务层

---
+++ src/main/java/com/mckay/service/BigFileMangeService.java
@@ -0,0 +1,21 @@
+/**   
+ * Copyright © 2017 本代码版权归周林波所有，严禁未经许可使用。
+ * 
+ * @Title: BigFileMangeService.java 
+ * @Prject: webplatform
+ * @Package: com.mckay.service 
+ * @Description:
+ * @author:
+ * @date: 2017年1月13日 下午10:26:03 
+ * @version: V1.0   
+ */
+package com.mckay.service;
+
+/** 
+ * @ClassName: BigFileMangeService 
+ * @Description: TODO
+ * @author: 周林波
+ * @date: 2017年1月13日 下午10:26:03  
+ */
+public class BigFileMangeService {
+}
