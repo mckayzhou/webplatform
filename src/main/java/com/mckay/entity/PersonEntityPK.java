9adb680371ddd8873cf0fc33a5fbafb2a545b252
mckayzhou <buctzhoulinbo@163.com>
2017-02-09 23:32:40 +0800

    实体类

---
+++ src/main/java/com/mckay/entity/PersonEntityPK.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright © 2017 本代码版权归周林波所有，严禁未经许可使用。
+ *
+ * @Author: 周林波
+ * @date: 2017/2/7 22:05
+ * @version: V1.0
+ */
+package com.mckay.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *@Description :TODO
+ *@Author: 周林波
+ *@Date :Created in 2017/2/7  22:05
+ */
+public class PersonEntityPK implements Serializable {
+    private int number;
+    private long salary;
+
+    @Column(name = "number")
+    @Id
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Column(name = "salary")
+    @Id
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonEntityPK that = (PersonEntityPK) o;
+
+        if (number != that.number) return false;
+        if (salary != that.salary) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = number;
+        result = 31 * result + (int) (salary ^ (salary >>> 32));
+        return result;
+    }
+}
