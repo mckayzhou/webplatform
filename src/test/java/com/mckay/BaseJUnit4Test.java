36d24dacc7c08ade962dbe0b007cf8a5beea2112
mckayzhou <buctzhoulinbo@163.com>
2017-02-10 18:37:37 +0800

    Spring框架测试基类，用于加载配置文件

---
+++ src/test/java/com/mckay/BaseJUnit4Test.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright © 2017 本代码版权归周林波所有，严禁未经许可使用。
+ *
+ * @Author: 周林波
+ * @date: 2017/2/10 18:23
+ * @version: V1.0
+ */
+package com.mckay;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *@Description :TODO
+ *@Author: 周林波
+ *@Date :Created in 2017/2/10  18:23
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:applicationContext.xml"})
+public class BaseJUnit4Test {
+}
