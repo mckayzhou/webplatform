f6ae8e2d5d4cfa9cd880de569b6d5608a9235844
mckayzhou <buctzhoulinbo@163.com>
2017-02-18 10:59:45 +0800

    用户信息相关常量

---
+++ src/main/java/com/mckay/constants/UserConstants.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright © 2017 本代码版权归周林波所有，严禁未经许可使用。
+ *
+ * @Author: 周林波
+ * @date: 2017/2/18 9:04
+ * @version: V1.0
+ */
+package com.mckay.constants;
+
+/**
+ *@Description :系统中使用的用户相关常量
+ *@Author: 周林波
+ *@Date :Created in 2017/2/18  9:04
+ */
+public class UserConstants {
+
+
+    //登录成功
+    public static  final String LOGIN_SUCCESS="success";
+
+    //登录失败
+    public static final String LOGIN_FAILURE="failure";
+
+    //用户未注册
+    public static final  String USER_NOT_REGISTER="notRegister";
+
+    //用户已存在
+    public static final String USER_EXIST="userExist";
+
+    //用户不存在
+    public static final String USER_NOT_EXIST="userNotExist";
+
+    //添加用户成功
+    public static final String REGISTER_SUCCESS="registerSuccess";
+
+    //添加用户失败
+    public static final String REGISTER_FAILURE="registerFailure";
+
+    //修改用户成功
+    public static final String EDIT_SUCCESS="editSuccess";
+
+    //修改用户失败
+    public static final String EDIT_FAILURE="editFailure";
+
+    //用户登录信息
+    public static  final String LOGIN_USER_INFO="loginUserInfo";
+}
